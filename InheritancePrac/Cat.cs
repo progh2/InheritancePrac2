73488d384984d726281ff57db1f24820a40ac6ba
Gihun Ham <ham@e-mirim.hs.kr>
2020-06-12 09:41:52 +0900

    Animal 부모 클래스를 만들어서 Dog와 Cat에서 상속받아 사용하도록 수정

--- InheritancePrac/Cat.cs
+++ InheritancePrac/Cat.cs
@@ -6,17 +6,11 @@ using System.Threading.Tasks;
 
 namespace InheritancePrac
 {
-    class Cat
+    class Cat : Animal
     {
-        public int Age { get; set; }
-
         public Cat()
         {
-            this.Age = 0;
         }
-
-        public void Eat() { Console.WriteLine("냠냠"); }
-        public void Sleep() { Console.WriteLine("ZzzZzz"); }
         public void Meow() { Console.WriteLine("냐옹"); }
     }
 }
--- InheritancePrac/Dog.cs
+++ InheritancePrac/Dog.cs
@@ -6,19 +6,14 @@ using System.Threading.Tasks;
 
 namespace InheritancePrac
 {
-    class Dog
+    class Dog : Animal
     {
-        public int Age { get; set; }
         public string Color { get; set; }
 
         public Dog()
         {
-            this.Age = 0;
             this.Color = "갈색";
         }
-
-        public void Eat() { Console.WriteLine("냠냠"); }
-        public void Sleep() { Console.WriteLine("ZzzZzz"); }
-        public void Bark() { Console.WriteLine("왕왕~"); }
+      public void Bark() { Console.WriteLine("왕왕~"); }
     }
 }
